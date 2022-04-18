69a594987c8dbc4571f4039ae68f1c60aba9d73b
Dayane Núbia <dayane.nubia67@gmail.com>
2022-04-18 13:12:05 -0300

    Update Operations.java

--- lista01/src/operations/Operations.java
+++ lista01/src/operations/Operations.java
@@ -1,27 +1,49 @@
+
 package operations;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Operations {
-	List<Integer> list = new ArrayList();
+
+    List<Integer> list = new ArrayList();
 	
 	public void addList(int amount, int number) {
-		for(int aux = 0; aux < amount; aux++) {
-			list.add(number);
-		}	
+            list.add(number);
 	}
 	
-	public int arithmeticList(int amount) {
-		int aux;
-		int soma = 0;
-		int resultado;
-		for(int a = 0; a < amount; a++) {
-			aux = this.list.get(a);
-			soma = soma + aux;
-		}
-		resultado = soma / amount;
-		return resultado;
+	public double arithmeticList(int amount) {
+            int aux;
+            int soma = 0;
+            double resultado;
+            for(int a = 0; a < amount; a++) {
+		aux = list.get(a);
+		soma = soma + aux;
+            }
+            resultado = soma / amount;
+            return resultado;
 	}
-
+        
+        public double geometricList(int amount){
+            int soma = 1;
+            int aux;
+            double resultado = 0;
+            for(int cont = 0; cont < amount; cont++){
+                aux = list.get(cont);
+                soma = soma * aux;
+                resultado = Math.sqrt(soma);
+            }
+            return resultado;
+        }
+        
+        public void evenOrOdd(int amount){
+            for(int cont = 0; cont < amount; cont++){
+                if(list.get(cont) % 2 == 0){
+                    System.out.println("Par: " + list.get(cont));
+                } else {
+                    System.out.println("Impar: " + list.get(cont));
+                }
+            }
+        }
+    
 }
