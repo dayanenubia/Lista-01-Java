792330f89367b635d45b5b907d9e454c568364ab
Dayane Núbia <dayane.nubia67@gmail.com>
2022-04-20 17:46:38 -0300

    Update Main.java

--- Matrix/src/main/Main.java
+++ Matrix/src/main/Main.java
@@ -8,33 +8,68 @@ public class Main {
     public static void main(String[] args) {
         
         Matrix matriz = new Matrix();
+        
         Scanner enter = new Scanner(System.in);
-        
-        System.out.print("Digite o tamanho da matriz 1: ");
+
+        System.out.println("**Digite o tamanho da matriz 1**");
+        System.out.print("Coluna: ");
 	int col1 = enter.nextInt();
+        System.out.print("Linha: ");
         int row1 = enter.nextInt();
-        int total1 = col1 * row1;
-        int number = 0;
         
-        System.out.print("Digite o tamanho da matriz 2: ");
-	
+        
+        System.out.println("**Digite o tamanho da matriz 2**");
+	System.out.print("Coluna: ");
 	int col2 = enter.nextInt();
+        System.out.print("Linha: ");
         int row2 = enter.nextInt();
-        int total2 = col2 * row2;
-        
+
         matriz.start(col1, row1, col2, row2);
         
-        for (int col = 0; col < col1; col++) {
-            for (int row = 0; row < row1; row++) {
-                System.out.println("Digite o numero: ");
-                number = enter.nextInt();
-                matriz.addOne(col1, row1, number);
+        matriz.addOne(col1, row1, row2);
+        
+        matriz.addTwo(col2, row2, row2);
+        
+        System.out.println("*** Escolha a opção  ***");
+	System.out.println("1 - Ver as matrizes");
+	System.out.println("2 - Ver a diagonal principal das matrizes");
+	System.out.println("3 - Ver a diagonal secundaria das matrizes");
+	System.out.println("4 - Sair\n");
+        
+	int option = enter.nextInt();
+
+        while (true) {
+            switch (option) {
+                case 1: {
+                    matriz.see1(col1, row1);
+                    matriz.see2(col2, row2);
+		}
+		break;
+                case 2: {
+                    matriz.principalDiagonalOne(col1, row1);
+                    matriz.principalDiagonalTwo(col2, row2);  		
+		}
+		break;
+                case 3: {
+                    matriz.secondaryDiagonalOne(col1, row1);
+                    matriz.secondaryDiagonalTwo(col2, row2);
+		}
+		break;
+                case 4: {
+                    System.out.println("Saindo...\n");
+                    return;
+                }
+                default:
+                    System.out.println("Opção inválida!\n");  
             }
+            System.out.println("*** Escolha a opção  ***");
+            System.out.println("1 - Ver as matrizes");
+	    System.out.println("2 - Ver a diagonal principal das matrizes");
+            System.out.println("3 - Ver a diagonal secundaria das matrizes");
+            System.out.println("4 - Sair\n");
+        
+            option = enter.nextInt();
         }
-        
-        
-        
-        matriz.see1(col1, row1);
         
     }
 }
